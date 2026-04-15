27bff0886f69b9c5de0285726d36e7978b22cbe7
xkliv <hamodek64@gmail.com>
2026-04-15 07:46:06 +0200

    Add pest and disease fields, update constructor

--- src/main/java/de/htwsaar/esch/Codeopolis/DomainModel/Plants/Conditions.java
+++ src/main/java/de/htwsaar/esch/Codeopolis/DomainModel/Plants/Conditions.java
@@ -12,17 +12,40 @@ public class Conditions {
 	private float averageTemperatureSummer;
 	private float averageTemperatureWinter;
 	
+	private boolean drought;
+	private boolean fusarium;
+	private boolean leafDrought;
+	private boolean powderyMildew;
+	private boolean barleyGoutFly;
+	private boolean deliaFly;
+	private boolean fritFly;
 	/**
      * Constructs a new `Conditions` object with specified values for soil conditions and temperatures.
      *
      * @param soilConditions         The soil conditions affecting plant growth (a float value between 0.0 and 1.0).
      * @param averageTemperatureSummer The average summer temperature (in degrees Celsius).
      * @param averageTemperatureWinter The average winter temperature (in degrees Celsius).
+	 * @param drought                  Whether there is a drought.
+     * @param fusarium                 Whether fusarium occurs.
+     * @param leafDrought              Whether leaf drought occurs.
+     * @param powderyMildew            Whether powdery mildew occurs.
+     * @param barleyGoutFly            Whether barley gout fly occurs.
+     * @param deliaFly                 Whether delia fly occurs.
+     * @param fritFly                  Whether frit fly occurs.
      */
-	public Conditions(float soilConditions, float averageTemperatureSummer, float averageTemperatureWinter ) {
+	public Conditions(float soilConditions, float averageTemperatureSummer, float averageTemperatureWinter,
+			boolean drought, boolean fusarium, boolean leafDrought, boolean powderyMildew,
+			boolean barleyGoutFly, boolean deliaFly, boolean fritFly) {
 		this.soilConditions = soilConditions;
 		this.averageTemperatureSummer = averageTemperatureSummer;
 		this.averageTemperatureWinter = averageTemperatureWinter;
+		this.drought = drought;
+		this.fusarium = fusarium;
+		this.leafDrought = leafDrought;
+		this.powderyMildew = powderyMildew;
+		this.barleyGoutFly = barleyGoutFly;
+		this.deliaFly = deliaFly;
+		this.fritFly = fritFly;
 	}
 	
     /**
@@ -51,6 +74,9 @@ public class Conditions {
 	public float getAverageTemperatureWinter() {
 		return averageTemperatureWinter;
 	}
+
+
+
 	
 	/**
 	 * Factory method to create a new Conditions object with random values for all fields.
